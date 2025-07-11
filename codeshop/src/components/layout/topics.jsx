7a072dbf4225f3d7f91cdb89d4b54dd5b148d790
Lucasmenezes08 <lms4@cesar.school>
2025-07-11 12:32:12 -0300

    :lipstick: style: Adicionar controle de estado para botão tópico selecionado.

--- codeshop/src/components/layout/topics.jsx
+++ codeshop/src/components/layout/topics.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import ButtonTopic from "../ui/button-topics"
 
 export default function Topics (){
+    const [btnSelect , setBtnSelect] = useState('todos');
+
+
+    function handleSelect (topicName){
+        setBtnSelect(topicName);
+    }
+
+
     return (
         <section className="flex flex-row gap-3 mt-[2rem] mb-[2rem] px-5">
-            <ButtonTopic  nome={'Todos'} tamanho={'medio'}/>
-            <ButtonTopic  nome={'FrontEnd'} tamanho={'grande'}/>
-            <ButtonTopic  nome={'BackEnd'} tamanho={'grande'}/>
-            <ButtonTopic  nome={'FullStack'} tamanho={'grande'}/>
-            <ButtonTopic  nome={'DataScience'} tamanho={'grande'}/>
+            <ButtonTopic  nome={'Todos'} tamanho={'medio'} onClick={() => handleSelect('Todos')} setColor={btnSelect === 'Todos'}/>
+            <ButtonTopic  nome={'FrontEnd'} tamanho={'grande'} onClick={() => handleSelect('FrontEnd')} setColor={btnSelect === 'FrontEnd'}/>
+            <ButtonTopic  nome={'BackEnd'} tamanho={'grande'} onClick={() => handleSelect('BackEnd')} setColor={btnSelect === 'BackEnd'}/>
+            <ButtonTopic  nome={'FullStack'} tamanho={'grande'} onClick={() => handleSelect('FullStack')} setColor={btnSelect ==='FullStack'}/>
+            <ButtonTopic  nome={'DataScience'} tamanho={'grande'} onClick={() => handleSelect('DataScience')} setColor={btnSelect === 'DataScience'}/>
         </section>
     )
 }
